6a26d22699cd1e5e0cd3eb8a49ddeef2b93ae958
James Francis <tcotav@gmail.com>
2014-05-30 10:17:37 -0700

    added application tests

--- spec/jdemo/jdemo_spec.rb
+++ spec/jdemo/jdemo_spec.rb
@@ -9,3 +9,19 @@ end
 describe port(8080) do
   it { should be_listening }
 end
+
+describe package('openjdk-7-jdk') do
+  it { should be_installed }
+end
+
+describe package('tomcat6') do
+  it { should be_installed }
+end
+
+describe file('/var/lib/tomcat6/webapps/punter.war') do
+  it { should be_file }
+end
+
+describe file('/var/lib/tomcat6/webapps/punter') do
+  it { should be_directory }
+end
